2f2dbdb6ef48ddbe84594e1b4d3815e0df7eb125
Your Name <your@email.com>
2026-09-12 17:06:56 +0100

    Implement Journal program for week02

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,64 @@
-using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+                Console.WriteLine($"\n{prompt}");
+                Console.Write("> ");
+                string response = Console.ReadLine();
+
+                Entry entry = new Entry
+                {
+                    _date = DateTime.Now.ToShortDateString(),
+                    _promptText = prompt,
+                    _entryText = response
+                };
+
+                journal.AddEntry(entry);
+                Console.WriteLine();
+            }
+            else if (choice == "2")
+            {
+                Console.WriteLine();
+                journal.DisplayAll();
+            }
+            else if (choice == "3")
+            {
+                Console.Write("What is the filename? ");
+                string filename = Console.ReadLine();
+                journal.LoadFromFile(filename);
+                Console.WriteLine();
+            }
+            else if (choice == "4")
+            {
+                Console.Write("What is the filename? ");
+                string filename = Console.ReadLine();
+                journal.SaveToFile(filename);
+                Console.WriteLine();
+            }
+            else if (choice == "5")
+            {
+                running = false;
+            }
+        }
     }
 }
